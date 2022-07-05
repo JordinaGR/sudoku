af2e139f19805960d7c1034c45065994d75b5351
Jordina Gavalda <grjordina@gmail.com>
2022-07-05 18:05:47 +0200

    new version

--- main2.py
+++ main2.py
@@ -1,7 +1,10 @@
-from cProfile import label
+from glob import glob
 from tkinter import *
-from sudoku.gridsfile import a, noneditedBoarda, b, noneditedBoardb
 import time
+
+from matplotlib.pyplot import text
+from gridsfile import a, noneditedBoarda, b, noneditedBoardb
+import numpy as np
 
 root = Tk()
 root.config(bg='white')
@@ -11,32 +14,35 @@ root.title("Sudoku")
 
 canvas = Canvas(root, width=540, height=540, bg='white')
 canvas.pack(side=BOTTOM)
+notsolvableLabel = Label(root, text=' ', bg='white')
+notsolvableLabel.place(x=200, y=10)
 
 labels = []
+delay = 1
 c = 0
 
-def printBoard(arr):
-    array = []
-    for i in range(9):
-        for j in range(9):
-            array.append(arr[i][j])
-
-    for x, l in zip(array, labels):
-        if x != -1:
-            l.config(text=str(x))
-        else:
-            l.config(text=' ')
-
-def printcell(x, y):
+def printcell(x, y): # given coordinates return index to acces labels
     return int(x*9 + y)
 
-def resetfunc():
-    global c
+def resetboard():  # reset whole board
+    global c, a, b
     c += 1
-    if (c % 2 == 0):
-        printBoard(noneditedBoarda)
+    if c % 2 == 0:
+        board = noneditedBoarda
     else:
-        printBoard(noneditedBoardb)
+        board = noneditedBoardb
+
+    notsolvableLabel.config(text=' ')
+
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] != -1:
+                labels[printcell(i, j)].config(text=board[i][j], font='Arial 18 bold')
+            if board[i][j] == -1:
+                            labels[printcell(i, j)].config(text=' ')
+
+    a = np.array(noneditedBoarda)
+    b = np.array(noneditedBoardb)
 
 def is_valid(grid, row, col, num):
     for i in range(9):
@@ -55,6 +61,15 @@ def is_valid(grid, row, col, num):
                 return False
     return True
 
+def paintsquare(x, y, color):
+    global canvas
+    canvas.create_rectangle(x*60, y*60, (x*60)+60, (y*60)+60, fill=color)
+    tx = labels[printcell(x, y)].cget('text')
+    print(tx)
+    labels[printcell(x, y)].config(text=tx)
+    canvas.update_idletasks()
+    time.sleep(delay)
+
 def solvesudoku(grid, row, col):
     if row == 8 and col == 9:
         return True
@@ -62,42 +77,54 @@ def solvesudoku(grid, row, col):
     if col == 9:
         col = 0
         row += 1
-
+    
     if grid[row][col] != -1:
         return solvesudoku(grid, row, col+1)
     
+    paintsquare(row, col, 'red')
+
     for i in range(1, 10):
         if is_valid(grid, row, col, i):
             grid[row][col] = i
+
+            paintsquare(row, col, 'white')
+            labels[printcell(row, col)].config(text=i, bg='white')  
+            canvas.update_idletasks()
+            time.sleep(delay)
 
             if solvesudoku(grid, row, col+1):
                 return True
 
         grid[row][col] = -1
+
+        paintsquare(row, col, 'white')
+
+
     return False
 
 def solvedef():
     global c
-    bo = b
     if c % 2 == 0:
         bo = a
+    else:
+        bo = b
+    
+    if (not solvesudoku(bo, 0, 0)):
+        notsolvableLabel.config(text='NOT SOLVABLE')
 
-    if (solvesudoku(bo, 0, 0)):
-        printBoard(bo)
-
-for i in range(9):
+for i in range(9):  # create all labels for each cell
     for j in range(9):
         l = Label(canvas, text=' ', font=('arial', 16), bg='white')
         l.place(x=j*60+20, y=i*60+15)
         labels.append(l)
 
-printBoard(a)
+resetboard()
 
 #graphics
-resetb = Button(root, text='RESET',  bg='white', command=resetfunc)
+resetb = Button(root, text='RESET',  bg='white', command=lambda:resetboard())
 resetb.place(x=20, y=10)
 
-solveb = Button(root, text='SOLVE',  bg='white', command=solvedef)
+solveb = Button(root, text='SOLVE',  bg='white', command=lambda:solvedef())
 solveb.place(x=100, y=10)
 
 canvas.create_line(0, 1, 540, 1, width=1)
